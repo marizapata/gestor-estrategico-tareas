9d1f25dcea1ddb4cc1aeb48bac3b40a46477accc
Mariana Zapata <marianazapata2398@gmail.com>
2026-09-25 19:25:17 -0500

    mostrar tareas en home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,12 @@
+import Tasks from './Tasks'
+
 function Home() {
   return (
     <main>
       <h1>Gestión de mi día a día</h1>
       <p>Organiza tus tareas y mantén el control de tu día.</p>
+
+      <Tasks />
     </main>
   )
 }
